53526c8b6e03c610fb56c066ce4014e95925ea31
Naveen Kalwan <138524861+kalwan23@users.noreply.github.com>
2024-08-31 13:07:08 +0530

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed=new Typed(".typing",{
-    strings:["Engineering Student"],
+    strings:["Engineering Student", "Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
